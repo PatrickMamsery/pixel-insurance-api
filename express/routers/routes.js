3907948140db2d33f81043f34b927804fa80cdcf
PatrickMamsery <pkrobert1612@gmail.com>
2023-09-12 23:47:33 +0300

    chore: complete setup of API scaffolding done and ready for implementation

--- express/routers/routes.js
+++ express/routers/routes.js
@@ -5,6 +5,10 @@ const router = express.Router();
 const AuthController = require('./../controllers/authController');
 const UserController = require('./../controllers/userController');
 const PackageController = require('./../controllers/insurancePackageController');
+const ClaimController = require('./../controllers/claimController');
+const ItemController = require('./../controllers/itemController');
+const CargoRecordController = require('./../controllers/cargoRecordController');
+const TransactionController = require('./../controllers/transactionController');
 
 // ================== MIDDLEWARE ==================
 // router.use(express.json());
@@ -18,6 +22,11 @@ router.get("/", (req, res) => {
 	// ================== USER ROUTES ==================
 	router.get("/api/users", UserController.index);
 	router.get("/api/users/:id", UserController.show);
+	router.post("/api/users", UserController.store);
+	router.put("/api/users/:id", UserController.update);
+
+	// Change password
+	router.put("/api/users/:id/change_password", UserController.changePassword);
 
 	// ================== AUTH ROUTES ==================
 	router.post("/api/register", AuthController.registerV2);
@@ -63,7 +72,34 @@ router.get("/", (req, res) => {
 	router.put("/api/claims/:id", ClaimController.update);
 	router.delete("/api/claims/:id", ClaimController.destroy);
 
-	// ================== ROUTES ==================
+	// ================== ITEM & CARGO RECORDS ROUTES ==================
+	// For Dashboard use
+	// Get all items
+	router.get("/api/items", ItemController.index);
 
+	// Get Cargo Records
+	router.get("/api/cargo_records", CargoRecordController.index);
+	router.get("/api/cargo_records/:id", CargoRecordController.show);
+
+	// For User use
+	// Get all items
+	router.get("/api/user/:id/items", ItemController.indexUserItems);
+
+	// Create item
+	router.post("/api/items", ItemController.store);
+	router.put("/api/items/:id", ItemController.update);
+	router.delete("/api/items/:id", ItemController.destroy);
+
+	// ================== TRANSACTION ROUTES ==================
+	// For Dashboard use
+	router.get("/api/transactions", TransactionController.index);
+	router.get("/api/transactions/:id", TransactionController.show);
+
+	// For User use
+	router.get("/api/user/:id/transactions", TransactionController.indexUserTransactions);
+	router.get("/api/user/:id/transactions/:id", TransactionController.showUserTransaction);
+	router.post("/api/transactions", TransactionController.store);
+
+	// ================== END MISC ROUTES ==================
 
 module.exports = router;
